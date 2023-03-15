5ecc2c9ae6e34e7796f147016fc8fd90bd9883a0
Krinopotam <omegatester@gmail.com>
2023-03-15 22:08:28 +0300

    dev

--- resources/app/components/baseComponents/tabulatorGrid/reactTabulator/parts/icons.tsx
+++ resources/app/components/baseComponents/tabulatorGrid/reactTabulator/parts/icons.tsx
@@ -3,12 +3,18 @@ import {Button} from 'baseComponents/button';
 import React from 'react';
 
 export const collapseSvg = reactToHtml(
-    <Button size={'small'} style={{width: 17, height: 17, lineHeight: '13px', padding: 0, marginRight: 5, borderRadius: 2}}>
+    <Button
+        size={'small'}
+        style={{width: 17, height: 17, lineHeight: '13px', paddingLeft: 0, paddingRight: 0, paddingBottom: 3, marginRight: 5, borderRadius: 2}}
+    >
         -
     </Button>
 );
 export const expandSvg = reactToHtml(
-    <Button size={'small'} style={{width: 17, height: 17, lineHeight: '13px', padding: 0, marginRight: 5, borderRadius: 2}}>
+    <Button
+        size={'small'}
+        style={{width: 17, height: 17, lineHeight: '13px', paddingLeft: 0, paddingRight: 0, paddingBottom: 3, marginRight: 5, borderRadius: 2}}
+    >
         +
     </Button>
 );
